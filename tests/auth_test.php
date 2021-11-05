4d08b6ea856b9e72280de83f6c286bfd726fcdd2
dylancen <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>
2021-11-05 15:36:02 -0400

    fixed tests

--- tests/auth_test.php
+++ tests/auth_test.php
@@ -1,5 +1,5 @@
 <?php
-  include 'auth.php';
+  include '../auth.php';
   $uri_token = generate_encoded_auth_token($_GET);
   echo "Token: $uri_token\n";
   echo verify_encoded_auth_token($uri_token, $_GET) ? "Token verified\n" : "Invalid token\n"; //correct token
--- tests/generate_token.php
+++ tests/generate_token.php
@@ -1,7 +1,7 @@
 <?php
-  include 'auth.php';
+  include '../auth.php';
   //echo var_dump($_GET);
-  $_GET = array("user_id" => 7, "website_id" => 1, "user_website_credentials" => "{ \"data\":\"encrypted_data\" }");
+  $_GET = array("user_id" => 8, "website_id" => 1, "user_website_credentials" => "{ \"data\":\"encrypted_data\" }");
   echo "\n\"";
   echo generate_encoded_auth_token($_GET);
   echo "\"\n";
