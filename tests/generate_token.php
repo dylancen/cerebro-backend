c22c6522d3ef35fc1a79b83a293fb488f4348251
dylancen <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>
2021-12-13 12:55:07 -0500

    changed test

--- tests/generate_token.php
+++ tests/generate_token.php
@@ -1,7 +1,8 @@
 <?php
   include '../auth.php';
   //echo var_dump($_GET);
-  $_GET = array("user_id" => "7");
+  //$_GET = array("username" => "badstudent@gmail.com", "public_key" => "password", "security_question" => "Have you done Jeffreys homework?", "security_answer" => "Obviously not!");
+  $_GET = array("user_id" => 7);
   echo "\n\"";
   echo generate_encoded_auth_token($_GET);
   echo "\"\n";
